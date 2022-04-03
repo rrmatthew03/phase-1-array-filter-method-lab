4183df3adef916c778d3b969a115ad0d2497e757
ryan rivera <rrmatthew03@yahoo.com>
2022-04-03 16:57:18 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+function findMatching(array, query){
+    console.log(query)
+    const result = array.filter(function(driver){
+    console.log(driver)
+    return(driver => query)
+    })
+console.log(result)    
+}
+
+function fuzzyMatch(array, query){
+    const drivers = []
+    console.log(drivers.length = 0)
+    const result = drivers.filter('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby')
+    console.log(fuzzyMatch(drivers, 'Sa')) === members(['Sammy', 'Sarah', 'Sally'])
+}
+
+
+
+function matchName(){}
